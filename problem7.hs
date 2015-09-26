8567f39c4de20ec4ebd6b48c7e01be29d83b8ef3
Louis Burke <louis.j.burke@gmail.com>
2015-09-26 14:52:00 +0100

    Implements problem 7

--- problem7.hs
+++ problem7.hs
@@ -3,3 +3,5 @@ module Flatten where
 data NestedList a = Elem a | List [NestedList a]
 flatten :: NestedList a -> [a]
 flatten (Elem x) = [x]
+flatten (List []) = []
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
