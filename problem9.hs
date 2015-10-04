8fe040ea50003b2b65d1bedd80e485731849de00
Louis Burke <louis.j.burke@gmail.com>
2015-10-04 16:12:06 +0100

    Implements problem 9

---
+++ problem9.hs
@@ -0,0 +1,8 @@
+module Pack where
+
+pack :: (Eq a) => [a] -> [[a]]
+pack [] = []
+pack [x] = [[x]]
+pack (x:xs) = if x `elem` (head (pack xs))
+              then (x:(head (pack xs))):(tail (pack xs))
+              else [x]:(pack xs)
